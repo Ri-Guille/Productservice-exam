15ad74879127681ff19eac52802b3c4f3d522c24
Ri-Guille <g.riverorebanal@colegiovirgendeeuropa.edu.es>
2025-10-29 09:54:18 +0100

    Tercer commit

--- src/com/guillermo/productservice/dao/ProductDAO.java
+++ src/com/guillermo/productservice/dao/ProductDAO.java
@@ -3,23 +3,41 @@
  */
 package com.guillermo.productservice.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.guillermo.productservice.model.Product;
+
 /**
  * @author G 29 oct 2025
  */
-public class ProductDAO {
-	private int id;
-    private String name;
-    private int stock;
+public interface ProductDAO {
 
-    public ProductDAO(int id, String name, int stock) {
-        this.id = id;
-        this.name = name;
-        this.stock = stock;
-    }
+    /**
+     * Obtiene un producto por su ID.
+     * * @param conn La conexión JDBC activa
+     * @param id   El ID del producto a buscar
+     * @return El objeto Product, o null si no se encuentra
+     * @throws SQLException si ocurre un error de SQL
+     */
+    Product getProductById(Connection conn, int id) throws SQLException;
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public int getStock() { return stock; }
-    public void setStock(int stock) { this.stock = stock; }
+    /**
+     * Obtiene un producto por su ID, bloqueando la fila para actualización.
+     * * @param conn La conexión JDBC activa
+     * @param id   El ID del producto a bloquear y buscar
+     * @return El objeto Product, o null si no se encuentra
+     * @throws SQLException si ocurre un error de SQL
+     */
+    Product getProductByIdForUpdate(Connection conn, int id) throws SQLException;
+
+    /**
+     * Actualiza un producto existente en la base de datos.
+     * * @param conn    La conexión JDBC activa
+     * @param product El producto con los datos actualizados
+     * @throws SQLException si ocurre un error de SQL
+     */
+    void updateProduct(Connection conn, Product product) throws SQLException;
+
 }
 
--- src/com/guillermo/productservice/model/Product.java
+++ src/com/guillermo/productservice/model/Product.java
@@ -7,35 +7,50 @@ package com.guillermo.productservice.model;
  * @author G 29 oct 2025
  */
 public class Product {
-	private int id;
+
+    private int id;
     private String productCode;
     private int stock;
     private String warehouse;
 
-    /**
-     * Constructor para inicializar un nuevo producto.
-     */
-    public Product(int id, String productCode, int stock, String warehouse) {
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
         this.productCode = productCode;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
         this.stock = stock;
+    }
+
+    public String getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(String warehouse) {
         this.warehouse = warehouse;
     }
 
-    // --- Getters ---
-    public int getId() { return id; }
-    public String getProductCode() { return productCode; }
-    public int getStock() { return stock; }
-    public String getWarehouse() { return warehouse; }
-
-    // --- Setters ---
-    public void setId(int id) { this.id = id; }
-    public void setProductCode(String productCode) { this.productCode = productCode; }
-    public void setStock(int stock) { this.stock = stock; }
-    public void setWarehouse(String warehouse) { this.warehouse = warehouse; }
-
     @Override
     public String toString() {
-        return "Product{id=" + id + ", code='" + productCode + "', stock=" + stock + '}';
+        return "Product{" +
+                "id=" + id +
+                ", stock=" + stock +
+                '}';
     }
 }
